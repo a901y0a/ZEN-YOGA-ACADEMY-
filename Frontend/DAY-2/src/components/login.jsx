c03ea7720849f424e1851b9cbc1262bbcd143359
Agalya <113664802+a901y0a@users.noreply.github.com>
2024-02-20 16:50:43 +0530

    Add files via upload

---
+++ Frontend/DAY-2/src/components/login.jsx
@@ -0,0 +1,129 @@
+import React, { useState } from 'react';
+import '../assets/login.css';
+
+function NewComponent() {
+  const [email, setEmail] = useState('');
+  const [mobileNumber, setMobileNumber] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const validateEmail = () => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  };
+
+  const validateMobileNumber = () => {
+    return mobileNumber.length === 10 && !isNaN(mobileNumber);
+  };
+
+  const validatePassword = () => {
+    return password.length >= 8;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    
+
+    if (email.trim() === '' || mobileNumber.trim() === '' || password.trim() === '') {
+      setErrorMessage('Please fill the details');
+      return;
+    }
+
+    if (!validateEmail()) {
+      setErrorMessage('Invalid email format');
+      return;
+    }
+    if (!validateMobileNumber()) {
+      setErrorMessage('Invalid mobile number');
+      return;
+    }
+    if (!validatePassword()) {
+      setErrorMessage('Password should be at least 8 characters long');
+      return;
+    }
+    
+
+    // All validations passed, you can proceed with form submission
+    setErrorMessage('');
+    // Your form submission logic goes here
+  };
+
+  return (
+    <>
+    <div style = {{ backgroundColor : "white"}}>
+      
+      
+      <div className="main"  style ={{backgroundColor: "white"}} >
+        <input type="checkbox" id="chk" aria-hidden="true" />
+        <div className="signup">
+          <form onSubmit={handleSubmit} >
+            <label htmlFor="chk" aria-hidden="true" style={{ marginBottom: "6px" , color: "lightblue"}}>
+              Sign up
+            </label>
+            <input
+              type="text"
+              name="txt"
+              placeholder="User name"
+              required=""
+            />
+            <input
+              type="text"
+              name=""
+              placeholder="Mobile number"
+              value={mobileNumber}
+              onChange={(e) => setMobileNumber(e.target.value)}
+              required=""
+            />
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required=""
+            />
+            <input
+              type="password"
+              name="pswd"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required=""
+            />
+            <button style={{ color: "black" }}>Sign up</button>
+            {errorMessage && <p style={{ textAlign: 'center',color: 'red' }}>{errorMessage}</p>}
+          </form>
+        </div>
+        <div className="login">
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="chk" aria-hidden="true" style={{ color: 'white' }} >
+              Login
+            </label>
+            <input
+              type="email"
+              name="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required=""
+            />
+            <input
+              type="password"
+              name="pswd"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required=""
+            />
+            <button style={{ color: "black" }}>Login</button>
+            {errorMessage && <p style={{ textAlign: "center", color: "red" }}>{errorMessage}</p>}
+          </form>
+        </div>
+      </div>
+      </div>
+    </>
+  );
+}
+
+export default NewComponent;
---
+++ Frontend/DAY-2/src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import App from './App.jsx'
+// import NewComponent from './components/login.jsx'
+import Navi from './components/nav.jsx'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Navi/>
+  </React.StrictMode>,
+)
